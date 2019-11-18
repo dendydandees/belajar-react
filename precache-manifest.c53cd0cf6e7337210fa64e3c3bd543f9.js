b7ebf909a5b052ed106b9bb20c92030670d8b3b9
dendydandees <dendydharmawanq@gmail.com>
2019-11-18 21:14:42 +0700

    Updates

---
+++ precache-manifest.c53cd0cf6e7337210fa64e3c3bd543f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79d415699ed584eaeb5ea6fdb1bb8cb",
+    "url": "/belajar-react/index.html"
+  },
+  {
+    "revision": "631133c867d484fa8343",
+    "url": "/belajar-react/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "fb00c46c8bb9782b87ae",
+    "url": "/belajar-react/static/js/2.34553e6c.chunk.js"
+  },
+  {
+    "revision": "631133c867d484fa8343",
+    "url": "/belajar-react/static/js/main.d2731c9f.chunk.js"
+  },
+  {
+    "revision": "3e52ca0d077767fb13db",
+    "url": "/belajar-react/static/js/runtime-main.038f0190.js"
+  }
+]);
